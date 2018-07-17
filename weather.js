79a9f547ea0d887f2b5d2f73fe47aa15257130a9
Noah Shrader <noahshrader@gmail.com>
2018-07-16 23:39:20 -0500

    Return weather data for Memphis zip code

---
+++ weather.js
@@ -0,0 +1,42 @@
+// Set default variables
+var apiKey = 'b72d10b7b527b805c5445dcce41e666c'
+var temp;
+var city;
+var img;
+
+function getWeather(zip) {
+  var query = 'http://api.openweathermap.org/data/2.5/weather?' + 'zip=' + zip + '&units=imperial&APPID=' + apiKey
+  getRequest(query)
+}
+
+function getRequest(query) {
+  var request = new XMLHttpRequest() // Get back JSON object
+  request.onreadystatechange = function() { // When we get data back, let's do something with it
+    if (request.readyState == 4 && request.status == 200) {
+      var data = JSON.parse(request.responseText)
+      var weather = {}
+      weather.img = data.weather[0].icon
+      weather.city = data.name
+      weather.temp = Math.round(data.main.temp)
+      update(weather)
+    }
+  }
+  request.open('GET', query, true)
+  request.send()
+}
+
+function update(weather) {
+  temperature.innerHTML = weather.temp
+  city.innerHTML = weather.city
+  image.src = 'https://openweathermap.org/img/w/' + weather.img + '.png'
+}
+
+window.onload = function () {
+
+  // Set elements
+  temp = document.getElementById('temperature')
+  city = document.getElementById('city')
+  img = document.getElementById('image')
+
+  getWeather(38133) // Memphis
+}
